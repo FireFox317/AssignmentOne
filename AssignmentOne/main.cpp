12adbf5ec9412311591fbc29326ef3acf5f62153
Timon Kruiper <Timon Kruiper@LAPTOP-3V2IS54Q>
2018-02-06 10:21:46 +0100

    Initial commit BMI

---
+++ AssignmentOne/main.cpp
@@ -0,0 +1,64 @@
+//==============================================================
+// Filename :
+// Authors :
+// Version :
+// License :
+// Description :
+//==============================================================
+
+#include <iostream>
+
+using namespace std;
+
+float weightUser, heightUser, BMI;
+
+void readUserData() {
+	cout << "Your weight (kg): ";
+	cin >> weightUser;
+	cout << "Your length (m): ";
+	cin >> heightUser;
+}
+
+void calculateBMI() {
+	BMI = weightUser / (heightUser*heightUser);
+}
+
+void evaluateAndPrintBMI() {
+	cout << endl << "Your BMI is " << BMI << endl;
+	cout << "This BMI corresponds to ";
+	if (BMI < 18.5) {
+		cout << "underweight";
+	}
+	else if (BMI >= 18.5 && BMI <= 24.9) {
+		cout << "normal";
+	}
+	else if (BMI > 25 && BMI <= 29.9) {
+		cout << "overweight";
+	}
+	else if (BMI > 30) {
+		cout << "obese";
+	}
+	cout << "!" << endl;
+}
+
+void printInfo() {
+	cout << "BMI Values:" << endl;
+	cout << "Underweight:\tless than 18.5" << endl;
+	cout << "Normal:\t\tbetween 18.5 and 24.9" << endl;
+	cout << "Overweight:\tbetween 25 and 29.9" << endl;
+	cout << "Obese:\t\t30 or greater" << endl;
+}
+
+int main() {
+	cout << "BMI Calculator" << endl << endl;
+
+	printInfo();
+	cout << endl;
+
+	readUserData();
+	calculateBMI();
+	evaluateAndPrintBMI();
+
+	return 0;
+}
+
