9fe909c34b1356ae69de4f7b33fb7124e369bbb9
Timon Kruiper <Timon Kruiper@LAPTOP-3V2IS54Q>
2018-02-07 15:49:59 +0100

    Removed BMI files and code

--- AssignmentOne/BMI.h
+++
@@ -1,9 +0,0 @@
-#ifndef BMI_H
-#define BMI_H
-
-void readUserData();
-void calculateBMI();
-void evaluateAndPrintBMI();
-void printInfo();
-
-#endif // !BMI_H
--- AssignmentOne/main.cpp
+++ AssignmentOne/main.cpp
@@ -7,22 +7,11 @@
 //==============================================================
 
 #include <iostream>
-
-#include "BMI.h"
 #include "Person.h"
 
 using namespace std;
 
 int main() {
-	//cout << "BMI Calculator" << endl << endl;
-
-	//printInfo();
-	//cout << endl;
-
-	//readUserData();
-	//calculateBMI();
-	//evaluateAndPrintBMI();
-
 	Person timon("Timon", "Kruiper", 31, 7, 1998);
 
 	cout << timon.getBirthDate() << endl;
